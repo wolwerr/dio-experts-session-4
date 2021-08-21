a0ee3c6e884d6ecd6c7163be8c7a2e8a60a8d6e6
wolwerr <ricardo@dtmm.com.br>
2021-08-20 23:04:19 -0300

    Initial commit

---
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/model/Product.java
@@ -0,0 +1,40 @@
+package one.digitalinnovation.experts.productcatalog.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "product", type = "catalog")
+
+public class Product {
+
+    @Id
+    private Long id;
+    private String name;
+    private Integer amount;
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+
+
+    }
+}
